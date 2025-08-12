6a4a397a21e1a57e0ecdc30c12dff97bc1ce4296
Fernanda Santiago <fernandamaria5501@gmail.com>
2025-08-12 11:29:01 -0300

    inicio: funcao

--- test/alunos/notas.py
+++ test/alunos/notas.py
@@ -1,1 +1,20 @@
-def
+def resultado(nota: float):
+
+
+    nota = [5, 0, 7, 8, 6, 10, 9]
+
+    media = sum(nota)/len(nota)
+    
+    if media < 5 & media >= 0:
+        print("REPROVADO")
+
+    if media >= 5 & media < 7:
+        print("RECUPERAÇÃO")
+
+    if media >= 7 & media <= 10:
+        print("APROVADO")
+
+
+ 
+   
+    return round(media,1)
