b1e4c830bd54279f4276d90cfdcf0affbd70ec20
Gabriela <severogabriela4@gmail.com>
2025-08-19 08:24:43 -0300

    add: primeiro teste

--- test/test_notas.py
+++ test/test_notas.py
@@ -1,3 +1,12 @@
 import pytest
 from alunos.notas import resultado
 
+def test_calcular_media_entrada_basica_sem_erro():
+    #Definindo as entradas
+    entrada = [5, 0, 7, 8, 6, 10, 9]
+
+    #Executo o código
+    resultado = resultado(entrada)
+
+    #checo a saída
+    assert resultado == 6.42
